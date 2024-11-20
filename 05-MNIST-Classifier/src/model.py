aebc21d0bb19c2e494bc3150cde0b05f3e65a9e5
vikashkumar <aiplaybook.in@gmail.com>
2024-11-20 16:53:54 -0500

    model architecture  updated

--- 05-MNIST-Classifier/src/model.py
+++ 05-MNIST-Classifier/src/model.py
@@ -9,15 +9,15 @@ class MNISTModel(nn.Module):
             nn.Conv2d(1, 8, kernel_size=3, padding=1),  # 28x28x8
             nn.ReLU(),
             nn.MaxPool2d(2),  # 14x14x8
-            nn.Conv2d(8, 12, kernel_size=1),  # 14x14x16
+            nn.Conv2d(8, 14, kernel_size=1),  # 14x14x16
             nn.ReLU(),
             nn.MaxPool2d(2),  # 7x7x16
-            nn.Conv2d(12, 16, kernel_size=1),  # 7x7x32
+            nn.Conv2d(14, 18, kernel_size=1),  # 7x7x32
         )
         
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(7 * 7 * 16, 10)
+            nn.Linear(7 * 7 * 18, 10)
         )
         
     def forward(self, x):
